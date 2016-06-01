6c4cf527336b542f5df8430fd2149c04ac50a2a9
Wayne Banks <thestorerun@yahoo.com>
2016-05-31 19:46:35 -0700

    finished app, without looking at solution once

---
+++ deaf_grandma.rb
@@ -0,0 +1,61 @@
+
+
+char='A'#Can be any character
+
+puts "Hi there Sonny! How are you!?"
+
+response = gets
+response = response.chomp
+
+if response===response.upcase then
+
+
+else
+    puts "What's that Sonny?"
+    gets
+end
+
+#Second question answer exchange
+#char='A'#Can be any character
+
+puts "No, not since 1928!"
+
+response = gets
+response = response.chomp
+
+if response===response.upcase then
+
+
+else
+    puts "What's that Sonny?"
+    gets
+end
+
+puts "No, not since 1928!"
+puts "Its getting late, dear."
+
+response = gets
+response = response.chomp
+
+if response===response.upcase then
+
+
+else
+    puts "What's that Sonny?"
+    gets
+end
+
+puts "No, not since 1928!"
+
+response = gets
+response = response.chomp
+
+if response===response.upcase then
+
+
+else
+    puts "What's that, Sonny?"
+    gets
+end
+puts "OKAY. GOODBYE, SONNY!"
+
